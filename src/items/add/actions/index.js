d9a7861fad05ff39e906fe26a3b0461d9cc7da0c
Mikhail Miropolskiy <the-ms@ya.ru>
2019-06-23 20:08:30 +0700

    исправления уязвимостей

--- src/items/add/actions/index.js
+++ src/items/add/actions/index.js
@@ -7,15 +7,15 @@ import { apiFetchWithAuth } from 'utils/api';
 import type { ItemType } from 'types/item';
 
 import {
-ITEMS_ADD_INIT,
-ITEMS_ADD,
-ITEMS_ADD_OK,
-ITEMS_ADD_ERROR,
+    ITEMS_ADD_INIT,
+    ITEMS_ADD,
+    ITEMS_ADD_OK,
+    ITEMS_ADD_ERROR,
 } from '../constants';
 
 export const initAdd = () => ({ type: ITEMS_ADD_INIT });
 
-export const add = (values:ItemType) => async (dispatch:Dispatch<Object>) => {
+export const add = (values: ItemType) => async (dispatch: Dispatch<Object>) => {
     dispatch({ type: ITEMS_ADD });
 
     try {
